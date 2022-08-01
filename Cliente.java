6bfedf1857725546e173724c4c8b7435d5f6bed5
Alberto Domingues <94317971+albertodomingues81@users.noreply.github.com>
2022-08-01 15:57:18 +0000

    Initial commit

---
+++ Cliente.java
@@ -0,0 +1,13 @@
+public class Cliente {
+  public String nome;
+
+  private String nome;
+
+  public String getNome() {
+    return nome;
+  }
+public void setNome(String nome) {
+  this.nome = nome;
+    }
+  
+}
